9c156e94f43b782521abfbd2e7204ff4fb2bc814
Elia Grayeb <77541181+elia1359@users.noreply.github.com>
2025-08-07 21:07:54 +0300

    Update rsa_factoring_shor_demo.py

--- rsa_factoring_shor_demo.py
+++ rsa_factoring_shor_demo.py
@@ -17,16 +17,16 @@ d = inverse(e, phi)
 m = 42
 c = pow(m, e, n)
 
-print("🔐 Public key (n, e):", (n, e))
-print("📤 Encrypted message:", c)
-print("📥 Decrypted message:", pow(c, d, n))
+print("Public key (n, e):", (n, e))
+print("Encrypted message:", c)
+print("Decrypted message:", pow(c, d, n))
 
 # Break n using Shor's Algorithm
-print("\n⚡ Factoring n =", n, "using Shor's Algorithm:")
+print("\n Factoring n =", n, "using Shor's Algorithm:")
 shor = Shor()
 backend = Aer.get_backend("aer_simulator")
 qi = QuantumInstance(backend, shots=1024)
 result = shor.factor(N=n, quantum_instance=qi)
-print("💥 Factors:", result.factors)
+print("Factors:", result.factors)
 
 # Verify the factors
